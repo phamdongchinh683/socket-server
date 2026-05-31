093a788c5aeb725ec8c9e968a9942a677f71d3e3
pham dong chinh <dchinh6803@gmail.com>
2026-06-01 02:09:49 +0700

    ..

--- src/routes/health.js
+++ src/routes/health.js
@@ -1,15 +1,11 @@
 const { getOnlineUsersCount, getOnlineMemoryStats } = require("../socket/online-store");
 
 async function sendHealthResponse(res, io) {
-    res.writeHead(200, {
-        "Content-Type": "application/json; charset=utf-8",
-        "Content-Length": Buffer.byteLength(body),
-    });
-    res.end(JSON.stringify({
-        status: "ok",
-    }))
-}
+    res.json({
+        status: "OK",
+    })
 
-module.exports = {
-    sendHealthResponse,
-};
+    module.exports = {
+        sendHealthResponse,
+    }
+}
